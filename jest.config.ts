8e627e9cff4783ea7d4c0a19b47735f5429990b9
fuadmns12 <fuadmns12@gmail.com>
2026-03-05 02:31:57 +0700

    upload

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import nextJest from 'next/jest.js'
+import type { Config } from 'jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/?(*.)+(test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/backup/'],
+}
+
+export default createJestConfig(config)
